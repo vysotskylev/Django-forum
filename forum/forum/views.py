9dbd74959b4c6e9b0007c0b98d753ed2a5a02185
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2015-03-06 16:12:52 +0300

    added basic login form

--- forum/forum/views.py
+++ forum/forum/views.py
@@ -1,8 +1,24 @@
 
 from django.shortcuts import render_to_response
 
+from django.http import HttpResponseRedirect
+
+from django.contrib import auth
+
 def login(request):
-    return render_to_response('login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            # Correct password, and the user is marked "active"
+            auth.login(request, user)
+            # Redirect to a success page.
+            return HttpResponseRedirect("/forum")
+        else:
+            return render_to_response('login.html', {'errors': 'Wrong login or username'})
+    else:
+        return render_to_response('login.html')
 
 def logout(request):
     return render_to_response('logout.html')
